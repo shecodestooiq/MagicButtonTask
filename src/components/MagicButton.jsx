19cbe9d744bf62e5718b9228599b3b0916f96f0e
zhraa <zhraa2005spn@gmail.com>
2023-11-01 20:33:02 +0300

    magic button task update

--- src/components/MagicButton.jsx
+++ src/components/MagicButton.jsx
@@ -9,8 +9,9 @@ function MagicButton({ children }) {
         ...children.props.style,
         backgroundColor: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
         color: `#${Math.floor(Math.random() * 16777215).toString(16)}`,
-        borderRadius: `${Math.floor(Math.random() * 50)}%`, 
+        borderRadius: `${Math.floor(Math.random() * 50)}%`,
       },
+
     });
     setButtons([...buttons, newButton]);
   };
